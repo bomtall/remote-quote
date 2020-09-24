883a2149eb3393cf628362c254f604e1eaf2f563
bomtall <bomtall1@hotmail.com>
2020-09-24 18:49:29 +0100

    add surface object with length and width dimensions

--- surface.py
+++ surface.py
@@ -1,8 +1,21 @@
 from numbers import Number
 
 
-class SurfaceArea:
+class SurfaceWithOnlyArea:
     def __init__(self, area):
         assert isinstance(area, Number), 'Input "area" needs to be numeric.'
         self.area = area
 
+
+class Surface(SurfaceWithOnlyArea):
+    def __init__(self, area=None, length=None, width=None):
+        if area is None:
+            assert length is not None and width is not None, 'Input either "area" or "length" and "width".'
+            assert isinstance(length, Number), 'Input "length" needs to be numeric.'
+            assert isinstance(width, Number), 'Input "width" needs to be numeric.'
+            area = length * width
+        else:
+            assert length is None and width is None, 'Input either "area" or "length" and "width".'
+        super().__init__(area)
+        self.length = length
+        self.width = width
--- test_surface.py
+++ test_surface.py
@@ -11,8 +11,8 @@ import surface
         ([], {'area': 1}, 1),
     ],
 )
-def test_surface(args, kwargs, expected):
-    s = surface.SurfaceArea(*args, **kwargs)
+def test_surface_with_only_area(args, kwargs, expected):
+    s = surface.SurfaceWithOnlyArea(*args, **kwargs)
     assert s.area == expected
 
 
@@ -23,7 +23,40 @@ def test_surface(args, kwargs, expected):
         ([], dict(area='a'), AssertionError, 'Input "area" needs to be numeric.'),
     ]
 )
+def test_surface_with_only_area_error(args, kwargs, error_type, error_message):
+    with pytest.raises(error_type) as e:
+        surface.SurfaceWithOnlyArea(*args, **kwargs)
+    assert e.value.args[0] == error_message
+
+
+@pytest.mark.parametrize(
+    'args, kwargs, expected',
+    [
+        ([1], dict(), 1),
+        ([None, 2, 2], dict(), 4),
+        ([], {'length': 2, 'width' : 2},  4),
+    ],
+)
+def test_surface(args, kwargs, expected):
+    s = surface.Surface(*args, **kwargs)
+    assert s.area == expected
+
+
+@pytest.mark.parametrize(
+    'args, kwargs, error_type, error_message',
+    [
+        ([1,2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
+        ([1, None, 2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
+        ([1, 3, 2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
+        ([], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
+        ([None, None, 2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
+        ([None, 2, None], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
+        ([None, '2', 2], dict(), AssertionError, 'Input "length" needs to be numeric.'),
+        ([None, 2, '2'], dict(), AssertionError, 'Input "width" needs to be numeric.'),
+
+    ]
+)
 def test_surface_error(args, kwargs, error_type, error_message):
     with pytest.raises(error_type) as e:
-        surface.SurfaceArea(*args, **kwargs)
+        surface.Surface(*args, **kwargs)
     assert e.value.args[0] == error_message
