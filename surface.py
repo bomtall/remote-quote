0211f4122472ebeb5c46d53e928a1f11c844a8cd
bomtall <bomtall1@hotmail.com>
2020-09-24 19:47:16 +0100

    added coverage adjustment

--- surface.py
+++ surface.py
@@ -2,7 +2,14 @@ from numbers import Number
 
 
 class Surface:
-    def __init__(self, area=None, length=None, width=None):
+    def __init__(
+            self,
+            area=None,
+            length=None,
+            width=None,
+            coverage_adjustment=None,
+    ):
+
         if area is None:
             assert length is not None and width is not None, 'Input either "area" or "length" and "width".'
             assert isinstance(length, Number), 'Input "length" needs to be numeric.'
@@ -11,6 +18,13 @@ class Surface:
         else:
             assert length is None and width is None, 'Input either "area" or "length" and "width".'
             assert isinstance(area, Number), 'Input "area" needs to be numeric.'
+
+        if coverage_adjustment is None:
+            coverage_adjustment = 1
+        else:
+            assert isinstance(coverage_adjustment, Number), 'Input "coverage_adjustment" needs to be numeric.'
+
         self.area = area
         self.length = length
         self.width = width
+        self.coverage_adjustment = coverage_adjustment
--- test_surface.py
+++ test_surface.py
@@ -5,16 +5,18 @@ import surface
 @pytest.mark.parametrize(
     'args, kwargs, expected',
     [
+        # Testing different numeric inputs
         ([1], dict(), 1),
         ([1.0], dict(), 1),
         ([1/2], dict(), 0.5),
+        # Testing keyword arguments
         ([], {'area': 1}, 1),
-        ([1], dict(), 1),
+        # Testing giving length and width
         ([None, 2, 2], dict(), 4),
-        ([], {'length': 2, 'width' : 2},  4),
+        ([], {'length': 2, 'width': 2},  4),
     ],
 )
-def test_surface(args, kwargs, expected):
+def test_surface_area(args, kwargs, expected):
     s = surface.Surface(*args, **kwargs)
     assert s.area == expected
 
@@ -22,20 +24,36 @@ def test_surface(args, kwargs, expected):
 @pytest.mark.parametrize(
     'args, kwargs, error_type, error_message',
     [
+        # Testing non numeric inputs
         (['a'], dict(), AssertionError, 'Input "area" needs to be numeric.'),
-        ([], dict(area='a'), AssertionError, 'Input "area" needs to be numeric.'),
-        ([1,2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
-        ([1, None, 2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
-        ([1, 3, 2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
-        ([], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
-        ([None, None, 2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
-        ([None, 2, None], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
         ([None, '2', 2], dict(), AssertionError, 'Input "length" needs to be numeric.'),
         ([None, 2, '2'], dict(), AssertionError, 'Input "width" needs to be numeric.'),
-
+        ([], dict(area=1, coverage_adjustment='a'), AssertionError, 'Input "coverage_adjustment" needs to be numeric.'),
+        # Testing when user only inputs two arguments but has to be one or three
+        ([1, 2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
+        ([1, None, 2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
+        # Testing if there are three inputs that the first is None
+        ([1, 3, 2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
+        # Testing for no inputs
+        ([], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
+        # Testing if only one input that it has to be area
+        ([None, None, 2], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
+        ([None, 2, None], dict(), AssertionError, 'Input either "area" or "length" and "width".'),
     ]
 )
 def test_surface_error(args, kwargs, error_type, error_message):
     with pytest.raises(error_type) as e:
         surface.Surface(*args, **kwargs)
     assert e.value.args[0] == error_message
+
+
+@pytest.mark.parametrize(
+    'args, kwargs, expected',
+    [
+        ([], dict(area=1, coverage_adjustment=1), 1),
+        ([], dict(area=1), 1),
+    ],
+)
+def test_surface_coverage_adjustment(args, kwargs, expected):
+    s = surface.Surface(*args, **kwargs)
+    assert s.coverage_adjustment == expected
